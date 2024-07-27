7cb9c32b7de7b3c4d2db9e0317680eeeba270d80
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 17:19:31 +0300

    no conversion from int to class if there is no conversion ctor

--- main.cpp
+++ main.cpp
@@ -1,82 +1,18 @@
 #include <iostream>
-#include <string>
-#include <fstream>
-#include <vector>
 
 class MyClass
 {
 public:
-	MyClass()
-	{
-		std::cout << "default ctor\n";
-		std::cout << "this "<< this << '\n';
-		std::cout << "-----------------------------\n";
-	}
+	MyClass() = default;
 
-	~MyClass()
-	{
-		std::cout << "destructor \n";
-		std::cout << "this " << this << '\n';
-		std::cout << "-----------------------------\n";
-	}
-
-	MyClass(int x)
-	{
-		std::cout << "MyClass(int) ctor\n";
-		std::cout << "this " << this << '\n';
-		std::cout << "-----------------------------\n";
-	}
-
-
-private:
-	
 };
-
-MyClass foo() //return PR value expression
-{
-	return MyClass{};
-}
-
-std::string foo2();
-
-void bar(std::string s)
-{
-	//s geçerli valid bir state e sahip
-}
-
-
-/*
-* 
-*to see how vector class have overloaded push_back for l and r values
-class Vector
-{
-public:
-	push_back(const string &);
-	push_back(string &&);
-
-};*/
 
 int main()
 {
+	//conversion ctor nedir?
 
-	using namespace std;
+	MyClass mx;
 
-	ifstream ifs{ "how-to-swim-faster.txt" };
-
-	string sline;
-
-	vector<string> svec;
-
-
-	while (getline(ifs, sline)) //bu halde getline sline 'ý moved from state halinde kullanýyor but sline is still valid bcs vector class leaves it still valid!
-	{
-		cout << sline << "\n";
-
-		//svec.push_back(sline);
-
-		svec.push_back(std::move(sline));
-
-		(void)getchar();
-	}
+	mx = 5; // no conversion enamled now
 
 }
