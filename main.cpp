264c47965409e90f6396cd12a2fb2785a2698099
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 21:37:19 +0300

    copy initialization is not valid for explicit ctor

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 class MyClass
 {
 public:
-	MyClass();
-	 void foo() noexcept; // static is not allowed
+	explicit MyClass(int);
+
 };
 
 void foo()
@@ -15,13 +15,8 @@ void foo()
 
 int main() 
 {
-	using namespace std;
-	//Follow c++ core guidelines
-	// //Tek paramlý ctorlarý aksi yönde sebep yoksa explicit yapmalýyýz!
-	// Bazý specifier lar bildirimde kullanýrsan tanýmda kullanmayacksýn ama bazýlarýný her iki yerde de kullanmalýsýn!
-	//explicit bildirimde varsa tanýmda olmayacak not allowed static function bildirilse de tanýmda satatic olmayacak
-	//noexcept hem bildirimde hem de tanýmda oalcak
+	MyClass m1(9); //direct initialization
+	MyClass m2{ 35 }; // direkt list initialization
+	MyClass m3 = 55; // copy initialization this is not valid for explicit ctor
 
-
-	
 }
