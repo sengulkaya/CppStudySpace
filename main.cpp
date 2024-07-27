c8b88a7312eac53a9289daed1c2ef233c0fe0b4c
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 18:20:57 +0300

    from pointer to bool conversion is implicit so bool to MyClass conversion is ok

--- main.cpp
+++ main.cpp
@@ -16,9 +16,13 @@ int main()
 {
 	//user defined conversion >> variadic conversion
 	MyClass m;
+	int x = 10;
+	int* ptr = nullptr;
 
 
-	m = false;// legal 
+	m = ptr; // if pointer is not null it is true if not it is false so pointer to bool conversion is implicit so this conversion is okay
+
+	m = &x;//  it is ok
 
 
 }
