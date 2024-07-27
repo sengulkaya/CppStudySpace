a678b9b4db086304287e4e06dd7ee15c6e2783d0
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 17:31:16 +0300

    see when dtor for temp object will be called

--- main.cpp
+++ main.cpp
@@ -4,7 +4,17 @@ class MyClass
 {
 public:
 	MyClass() = default;
-	MyClass(int);
+	MyClass(int x)
+	{
+		std::cout << "MyClass (int) x: " << x << "\n";
+		std::cout << "this pointer value : " << this << "\n";
+	}
+
+	~MyClass()
+	{
+		std::cout << "MyClass dtor "<< "\n";
+		std::cout << "this pointer value : " << this << "\n";
+	}
 
 };
 
@@ -12,10 +22,16 @@ int main()
 {
 	//conversion ctor nedir?
 
-	MyClass mx;
+	MyClass mx; //default ctor will be called
+
+	std::cout << "main before mx = 5 " << "\n";
 
 	mx = 5; //it is ok now because this will implicitly calling  MyClass(int) and it will give us temporary object 
 	//and then MyClass move assignment operator will be called 
+
+	std::cout << "main after mx = 5 " << "\n";
+
+	//since temporry object will have life extention it is lifetime depends on mx so dtor will be called after std::cout << "main after mx = 5 " << "\n"; line
 	
 
 }
