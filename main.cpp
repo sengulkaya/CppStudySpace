2e1c061fbfcb90c5287df61988cef8837de3fcba
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 18:16:45 +0300

    from double to MyClass by conversion

--- main.cpp
+++ main.cpp
@@ -3,52 +3,22 @@
 class MyClass
 {
 public:
-	MyClass() = default;
-	MyClass(int x)
-	{
-		std::cout << "MyClass (int) x: " << x << "\n";
-		std::cout << "this pointer value : " << this << "\n";
-	}
-
-	MyClass& operator=(const MyClass& ref)
-	{
-		std::cout << "copy assignment this : " << this << "\n"; 
-		std::cout << "ref pointer value : " << &ref << "\n";
-		return *this;
-	}
-
-
-	MyClass& operator=(MyClass&& ref)
-	{
-		std::cout << "move assignment this : " << this << "\n"; 
-		std::cout << "ref pointer value : " << &ref << "\n";
-		return *this;
-	}
-
-	~MyClass()
-	{
-		std::cout << "MyClass dtor "<< "\n";
-		std::cout << "this pointer value : " << this << "\n";
-	}
+	MyClass();
+	MyClass(int);
+	
+	
 
 };
 
+void func(MyClass);
+
 int main() 
 {
-	//what if you just add a copy assignment then 
+	//user defined conversion >> variadic conversion
+	MyClass m;
 
 
-
-	MyClass mx;
-
-	std::cout << "mx address :  " << &mx << "\n";
-
-	std::cout << "main before mx = 5 " << "\n";
-
-	mx = 5; 
-
-	std::cout << "main after mx = 5 " << "\n";
-
+	m = 4.9073;// legal 
 
 
 }
