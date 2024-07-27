0d0b3f17fbdf39aae3a0447143de454617f3e26b
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 16:13:49 +0300

    move assign.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
+#include <cstdlib>
+
+class MyClass
+{
+public:
+	MyClass()
+	{
+		std::cout << "default ctor\n";
+		std::cout << "this "<< this << '\n';
+		std::cout << "-----------------------------\n";
+	}
+
+	~MyClass()
+	{
+		std::cout << "destructor \n";
+		std::cout << "this " << this << '\n';
+		std::cout << "-----------------------------\n";
+	}
+
+	MyClass(int x)
+	{
+		std::cout << "MyClass(int) ctor\n";
+		std::cout << "this " << this << '\n';
+		std::cout << "-----------------------------\n";
+	}
+
+
+private:
+	
+};
+
+MyClass foo() //return PR value expression
+{
+	return MyClass{};
+}
+
+std::string foo2();
+
+void bar(std::string s)
+{
+	//s geçerli valid bir state e sahip
+}
+
 
 int main()
 {
-	std::cout << "hello it is me Þengül";
+
+	using namespace std;
+
+	string s;
+
+	string str(200'00, 'Z');
+
+	string sx = str; // here it is by copy ctor sx will have a life ama mesela str hayatýný devam ettirmesine raðmen ben onun kaynaðýný çaðýrmak istiyorsam;
+
+	string sx_move = std::move(str); // burada artýk str will be an r value gel beni çal diyor str
+	//1 burada daha str dtor çaðrýlmamýþtýr. 
+
+
+
+	s = foo2();
+
 }
