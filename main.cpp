9eda9a4b4333c2c5b14797ebedd816fd45039e5d
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 20:29:16 +0300

    c string param alan user defined conversion ctor örneği

--- main.cpp
+++ main.cpp
@@ -1,38 +1,22 @@
 #include <iostream>
 
-class A
+class String
 {
 public:
-	A(int);
+	String();
+	String(const char * p);
 
 };
 
-class B
-{
-public:
-	B();
-	B(A); //conversion ctor
 
-};
+
 
 
 int main() 
 {
-	/*
-	* Eðer conversion is done by one of the folllowings sequences , it must be done by compiler implicity
-	* 1 - user defined conversion + standard conversion 
-	* 2 - standard conversion + user defined conversion 
-	* 
-	* 
-	* 
-*/
+	String name;
 
-	
-	B bx; 
-	bx = static_cast<A> (13);
-	// this can not happen becuse compiler wont do two user defined conversion
-
-	//but user defined conversion & udc will not be done by compiler implicitly
+	name = "Þengül"; // valid bcs 7 row conversion stor sayesinde user defined yani 
 
 
 }
