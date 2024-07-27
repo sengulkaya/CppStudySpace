afa8eb01e56dafa241b0e992f7d57498727a4dd2
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 21:22:04 +0300

    you can use  static_cast to use explicit ctors

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
 	using namespace std;
 	// so by using explicit keyword for ctor we wont let compiler do user defined conversion implicitly
 	// so thanks to this 
-	MyClass m = 35; /// err it says noo suitable ctor 
+	MyClass m = static_cast<MyClass> (35) ; /// err it says noo suitable ctor 
 
 	
 }
