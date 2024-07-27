e4cc9d441b2cfe4cabab081e5bb0403f207763ad
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 23:45:31 +0300

    BOOM copy ctor is NOT called! in case call by value of foo with temp object

--- main.cpp
+++ main.cpp
@@ -1,31 +1,59 @@
 #include <iostream>
-#include <vector>
-#include <memory>
+
 class MyClass
 {
 public:
-	explicit MyClass(int, int);
+	MyClass()
+	{
 
+	 std::cout << "default ctor" << "\n";
+	}
+
+	explicit MyClass(int x) // making this explicit common practice
+	{
+		std::cout << "MyClass(int x) ctor" << "\n";
+
+	}
+
+	~MyClass()
+	{
+		std::cout << "DESTRUCTOR" << "\n";
+
+	}
+
+
+	/*MyClass(const MyClass& ref)
+	{
+		std::cout << "copy ctor" << "\n";
+
+	}*/
+
+	MyClass & operator=(const MyClass& ref)
+	{
+		std::cout << "copy assignment" << "\n";
+		return *this;
+	}
+
+
+	MyClass(MyClass&& ref)
+	{
+		std::cout << "move ctor" << "\n";
+	}
+
+	/*MyClass & operator=(MyClass&& ref)
+	{
+		std::cout << "move assignment" << "\n";
+		return *this;
+	}*/
 };
 
-void foo(MyClass)
+void foo(MyClass x) // call by value. so move ctor will be called for x. but what if we eliminate move ctor then copy ctor will be called(BOOM copy ctor is NOT called!)
 {
-	//
-	
+	std::cout << "foo()\n";
 }
-
-
-MyClass bar()
-{
-	return {45, 78};//valid; making ctor explicit this becomes error
-
-}
-
 
 int main() 
 {
-	
-	MyClass m = { 12, 67 };
-	foo({ 12, 67 }); //valid; making ctor explicit this becomes error
-
+	//copy elision ; elision is like elimination
+	foo(MyClass{}); 
 }
