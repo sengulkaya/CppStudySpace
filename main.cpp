1c4288788b9b6cdded2d252fc123d2dec86c2863
sengulkaya <sengulkaya15@yahoo.com>
2024-07-27 18:42:03 +0300

    compiler wont do two user defined conversion

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
-class MyClass
+class A
 {
 public:
-	MyClass();
-	MyClass(int);
-	
-	
+	A(int);
+
+};
+
+class B
+{
+public:
+	B();
+	B(A); //conversion ctor
 
 };
 
@@ -18,12 +23,16 @@ int main()
 	* Eðer conversion is done by one of the folllowings sequences , it must be done by compiler implicity
 	* 1 - user defined conversion + standard conversion 
 	* 2 - standard conversion + user defined conversion 
+	* 
+	* 
+	* 
 */
-	MyClass m;
-	double dval = 32.5;
-	
 
-	m = dval;//  it is ok
+	
+	B bx;
+	bx = 13; // this can not happen becuse compiler wont do two user defined conversion
+
+	//but user defined conversion & udc will not be done by compiler implicitly
 
 
 }
