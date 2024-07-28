7dd2288d5c0f9ceb3757923508da38b4f0cdcf91
sengulkaya <sengulkaya15@yahoo.com>
2024-07-29 00:50:36 +0300

    i see every address of MyClass is increased by 256 so i can observe hexadecimal addresses change at the third position.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,92 @@
 #include <vector>
 #include <iostream>
 
+#include <vector>
+#include <iostream>
+class MyClass
+{
+public:
+	MyClass()
+	{
+
+		std::cout << "default ctor" << this << "\n";
+	}
+
+	explicit MyClass(int x) // making this explicit common practice
+	{
+		std::cout << "MyClass(int x) ctor" << this << "\n";
+
+	}
+	explicit MyClass(int x, int y) // making this explicit common practice
+	{
+		std::cout << "MyClass(int x, int y) ctor x = " << x << "y= " << y << "\n";
+
+	}
+
+
+	~MyClass()
+	{
+		std::cout << "dtor" << this << "\n";
+
+	}
+
+
+	MyClass(const MyClass& ref)
+	{
+		std::cout << "copy ctor" << this << "\n";
+	}
+
+
+	MyClass& operator=(const MyClass& ref)
+	{
+		std::cout << "copy assign" << this << "\n";
+	}
+
+	MyClass(MyClass&& ref)
+	{
+		std::cout << "move ctor" << this << "\n";
+	}
+
+
+	MyClass& operator=(MyClass&& ref)
+	{
+		std::cout << "move assign" << this << "\n";
+	}
+
+	void foo()
+	{
+		std::cout << "MyClass foo" << this << "\n";
+	}
+
+	char buf[256]{};
+
+
+};
+
+void* operator new(std::size_t sz)
+{
+	std::cout << "senguls operator new is called" << "\n";
+
+	void* vp = malloc(sz);
+	if (!vp)
+	{
+		throw std::bad_alloc{};
+	}
+	std::cout << "address of allocated block is" << sizeof(MyClass) << vp << "\n";
+	return vp;
+
+}
+
+void operator delete(void* vp)
+{
+	std::cout << "senguls operator delete is called" << vp << "\n";
+	std::free(vp);
+}
 
 
 
 int main() 
 {
 	
-
-	std::cout << "Kaç tamsayý : ";
-
-	std::size_t n;
-
-	std::cin >> n; 
-	 
-	int* p = new int[n];// array new
-
-	for (std::size_t i{}; i < n; i++)
-	{
-		p[i] = i;
-	}
-	
-
-	for (std::size_t i{}; i < n; i++)
-	{
-		std::cout << p[i] << " " << '\n';
-	}
-
-	delete[] p; //array delete
+	MyClass* p = new MyClass[10];
 }
