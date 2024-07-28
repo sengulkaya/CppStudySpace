88846b6e63df55213472289252676697497aa0e8
sengulkaya <sengulkaya15@yahoo.com>
2024-07-28 22:47:06 +0300

    operator new throwing bad allocation exception

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 
-
+std::vector<void *> gvec;
 
 int main() 
 {
+	using namespace std;
+
+	int i; 
 
 
-	void * operator new (std::size_t ) // malloc gibi dynamic bellek alanýnda yer ediniyor . baþarýsýz olursa exception gönderiyor 
+	try
+	{
+
+		for (int i = 0; i < 10000000000000; ++i)
+		{
+			gvec.push_back(operator new (1024 * 1024));
+			std::cout << "." << "\n";
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << "exception caught: " << ex.what() << '\n'; 
+}
 
 }
