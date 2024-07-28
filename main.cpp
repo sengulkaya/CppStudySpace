36176506d98148499d3c2be9f6e50a714901f4ba
sengulkaya <sengulkaya15@yahoo.com>
2024-07-28 18:02:21 +0300

    mandatory copy elision ve complier optimization olmadığında C++17 öncesi temp objects return edebilmek için move ctor eğer o declared değilse copy ctora ihtiyac var.

--- main.cpp
+++ main.cpp
@@ -22,12 +22,14 @@ public:
 	}
 
 
-	MyClass(const MyClass& ref)
-	{
-		std::cout << "copy ctor" << "\n";
-	}
+	MyClass(const MyClass& ref) = delete;
 
-	
+/*
+
+		{
+		std::cout << "copy ctor" << "\n";
+
+
 	MyClass & operator=(const MyClass& ref)
 	{
 		std::cout << "copy assign" << "\n";
@@ -44,18 +46,31 @@ public:
 		std::cout << "move assign" << "\n";
 	}
 
+	}*/
+
+	
+	
+
 
 };
 
-void foo(MyClass x) 
+MyClass foo(int n) 
 {
-	std::cout << "foo()\n";
+	//code
+	return MyClass{n};
 }
 
 int main() 
 {
+	//RVO
 
-	foo(MyClass{ 45});
+	MyClass m = foo(13);// What will happen before c++17 ? 
+	//Fonksiyon return value pr value olduðu için initialize durumunda (MyClass m = foo(13);)  temporary materialization gerçekleþmediði sürece nesne oluþmayacak ki burada oluþuyor.
+	// C++17 öncesi durmunda optimization olmadýðýnda ve copy ctor delete olduðunda hata veriyor. Neden peki move ctor çaðrýlacak ama biz copy delete olmasý diyoruz? 
+	// çünkü copy ctor declare edilmiþ bu durumda move ctor sen yazmadýðýn sürece NOT declared ama gel gör ki olan copy ctor da fall back ile kullanýlmasý gerekirken deleted halde o yüzde özellikle örnek böyle veriliyor.
+	// Burada attempted copy ctor is deleted diye error veriyor.
+	//
+
 
 
 }
