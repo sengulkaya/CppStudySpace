48d3d28eea3a31517568603a489f4429870e22ec
sengulkaya <sengulkaya15@yahoo.com>
2024-07-28 22:49:53 +0300

    how new operates

--- main.cpp
+++ main.cpp
@@ -7,21 +7,8 @@ int main()
 {
 	using namespace std;
 
-	int i; 
+	new Fighter //static_cast<Fighter *>(operator new (sizeof(Fighter))) ->Fighter() 
 
-
-	try
-	{
-
-		for (int i = 0; i < 10000000000000; ++i)
-		{
-			gvec.push_back(operator new (1024 * 1024));
-			std::cout << "." << "\n";
-		}
-	}
-	catch (const std::exception& ex)
-	{
-		std::cout << "exception caught: " << ex.what() << '\n'; 
-}
+		
 
 }
