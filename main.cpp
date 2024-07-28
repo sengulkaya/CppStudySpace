10c71915967eafc4f391753558f5dbe79474b359
sengulkaya <sengulkaya15@yahoo.com>
2024-07-28 22:54:56 +0300

    how do you assign object created with new operator

--- main.cpp
+++ main.cpp
@@ -1,14 +1,57 @@
 #include <vector>
 #include <iostream>
+class MyClass
+{
+public:
+	MyClass()
+	{
 
-std::vector<void *> gvec;
+		std::cout << "default ctor" << this <<  "\n";
+	}
+
+	explicit MyClass(int x) // making this explicit common practice
+	{
+		std::cout << "MyClass(int x) ctor" << this << "\n";
+
+	}
+
+	~MyClass()
+	{
+		std::cout << "DESTRUCTOR" << this << "\n";
+
+	}
+
+
+	MyClass(const MyClass& ref)
+	{
+		std::cout << "copy ctor" << this << "\n";
+	}
+
+
+	MyClass& operator=(const MyClass& ref)
+	{
+		std::cout << "copy assign" << this << "\n";
+	}
+
+	MyClass(MyClass&& ref)
+	{
+		std::cout << "move ctor" << this << "\n";
+	}
+
+
+	MyClass& operator=(MyClass&& ref)
+	{
+		std::cout << "move assign" << this << "\n";
+	}
+
+
+};
+
 
 int main() 
 {
-	using namespace std;
-
-	new Fighter //static_cast<Fighter *>(operator new (sizeof(Fighter))) ->Fighter() 
-
-		
-
+	MyClass* p1 = new MyClass;
+	std::cout << "p1: " << &p1 << "\n";
+	auto p2 = new MyClass;
+	auto *  p3 = new MyClass;
 }
