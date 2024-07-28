bfc9ffb8d6082a356d03c3e5b22890d07ed0e8d9
sengulkaya <sengulkaya15@yahoo.com>
2024-07-28 22:34:51 +0300

    operator new

--- main.cpp
+++ main.cpp
@@ -1,28 +1,12 @@
 #include <iostream>
 #include <vector>
 
-class MoveOnly
-{
-public:
-	MoveOnly() = default;
-	MoveOnly(const MoveOnly&) = delete;
-	MoveOnly & operator=(const MoveOnly&) = delete; //You could just declare move assignment and move ctor to get deleted copy ctor and copy assignment but some chose to write it explicity like this
-	MoveOnly(MoveOnly&&);
-	MoveOnly& operator=(MoveOnly&&);
-
-};
-
-MoveOnly foo()
-{
-	MoveOnly x;
-
-	return x;// pessimistic move which makes copy elision not available
-
-}
 
 
 int main() 
 {
-	MoveOnly x(foo()); // here there is NO copy elision ? but why
+
+
+	void * operator new (std::size_t ) // malloc gibi dynamic bellek alanýnda yer ediniyor . baþarýsýz olursa exception gönderiyor 
 
 }
