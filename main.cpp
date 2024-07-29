8a3b1f99a51530d111d7532cfc1f9e86f6e0ee3f
sengulkaya <sengulkaya15@yahoo.com>
2024-07-29 21:08:38 +0300

    you cannot dereference incomplete type

--- main.cpp
+++ main.cpp
@@ -1,41 +1,30 @@
-#include <vector>
 #include <iostream>
-//#include <neco.h>
-struct Data
+
+
+class Nec;
+
+class Mert;
+
+class Nec
 {
-	int mx;
-	struct Data sData;// incomplete type struct is NOT allowed in struct; any class and struct can not have its own type as its member but it is possible if it is static
+public:
+
+
+	// void is an incomplete type
+
+private:
+	Mert mert; // incomplete type is not allowed
 };
 
-class Data
+Mert* foo();
+
+int main()
 {
-	int mx;
-	static Data sData;// incomplete type struct is NOT allowed in struct; any class and struct can not ahve its own type as its member but it is possible if it is static
-};
+	Mert m_mert; // you can not initilize incomplete type; no definition
 
 
+	Mert* m = foo();
 
-int main() 
-{
-	// what can we do with incomplete type? 
-	// Function declaration
-	//type alias declaration
-
-	typedef Neco* NecoPtr;
-	typedef Neco& NecoRef;
-	using NecoPtr = Neco*; // daha kolay sezgisel compared to typedef. and useful for generic programming paradign 
-
-	//Pointer ya da referans deðiþkenler tanýmlayabiliriz
-
-
-	Ali * aliPtr =  nullptr; // no error
-
-	Ali* p = foo();
-	Ali& ref = bar();
-	baz(ref);
-
-
-
-	
+	&*m; //it is ok
 
 }
