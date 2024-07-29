bbb41f68d6f9107b55f5780019c1fe84ab0d5c9c
sengulkaya <sengulkaya15@yahoo.com>
2024-07-29 21:11:46 +0300

    error bcs no definition of static x

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 
 
-class Nec;
-
-class Mert;
-
 class Nec
 {
 public:
-
+	static int x;
 
 	// void is an incomplete type
 
 private:
-	Mert mert; // incomplete type is not allowed
+	
 };
 
-Mert* foo();
 
 int main()
 {
-	sizeof(Mert);
+	Nec::x = 5;
 
 }
