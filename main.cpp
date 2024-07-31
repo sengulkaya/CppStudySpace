2e90505956ef2b9b2da9257545dec7a0e6166dab
sengulkaya <sengulkaya15@yahoo.com>
2024-07-31 13:01:22 +0300

    add is not accessible bcs it is declared static in another file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-static int g_x{}; // non-constant globals have external linkage by default, but can be given internal linkage via the static keyword
-
-const int g_y{ 1 }; // const globals have internal linkage by default
-constexpr int g_z{ 2 }; // constexpr globals have internal linkage by default
+int add(int x, int y); // forward declaration for function add
 
 int main()
 {
-    std::cout << g_x << ' ' << g_y << ' ' << g_z << '\n';
+    std::cout << add(3, 4) << '\n';
+
     return 0;
 }
