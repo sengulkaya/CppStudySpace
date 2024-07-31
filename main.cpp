12110106716f87b25320f848b610a2efae046974
sengulkaya <sengulkaya15@yahoo.com>
2024-07-31 12:47:27 +0300

    non-constant globals have external linkage by default, but can be given internal linkage via the static keyword
    // const globals have internal linkage by default
    
    // constexpr globals have internal linkage by default

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 
+static int g_x{}; // non-constant globals have external linkage by default, but can be given internal linkage via the static keyword
 
-class Nec
-{
-public:
-	static int mx ;
-
-
-	
-};
-
+const int g_y{ 1 }; // const globals have internal linkage by default
+constexpr int g_z{ 2 }; // constexpr globals have internal linkage by default
 
 int main()
 {
-
-	Nec mnec;
-	Nec* p{ &mnec };
-	p->mx;
-	mnec.mx; //nesne elemaný deðil sýnýf elemaný
+    std::cout << g_x << ' ' << g_y << ' ' << g_z << '\n';
+    return 0;
 }
