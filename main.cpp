bd192d953868e7fd5f94a3db2f98279f48879f9c
sengulkaya <sengulkaya15@yahoo.com>
2024-07-31 21:40:23 +0300

    where static data member can be set

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 
-int add(int x, int y); // forward declaration for function add
+class Nec
+{
+public:
+    int x;
+    static int mx;
 
+    void foo() const // bu þu demek void foo(const Nec * ) 
+    {
+        mx = 6; // this is NOT error but it is for nonstatic data members of const object
+       // x = 7; ERROR
+    }
+
+    void func()
+    {
+        mx = 10;
+        this->mx = 45;
+    }
+};
+
+
+int Nec::mx{};
 int main()
 {
-    std::cout << add(3, 4) << '\n';
+    Nec n1;
+    Nec n2;
+    Nec n3;
+
+
+
+   ++ n1.mx;
+   ++n2.mx;
+   ++n3.mx;
+
+   n1.func();
+
+   std::cout << "mx" << Nec::mx << '\n';
+
 
     return 0;
 }
