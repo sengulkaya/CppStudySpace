cffec555ef38abb082a33aacf858aa9cc30db9ee
sengulkaya <sengulkaya15@yahoo.com>
2024-08-01 21:11:56 +0300

    non-static data memeber in static func

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ class Nec
 public:
     static void foo() const //ERR static function has no hidden object param
     {
-        Nec mynec;
-        mynec.mx = 5;
+        mx;//ERROR  name lookup ile mx bulunur ama bunu nitelyen bir object yok. O yüzden err
+        Nec nec;
+        nec.mx = 5; //OK
     }
     
 
