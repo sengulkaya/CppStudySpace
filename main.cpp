8a93eb121184ebe2a20e4e610fb86832f3ec6e96
sengulkaya <sengulkaya15@yahoo.com>
2024-08-02 00:16:47 +0300

    static factory functions instead of ctors to create objects

--- main.cpp
+++ main.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 
-class Nec
+class MyClass
 {
 public:
-    static double foo()
-    {
-        return 3.9;
-    }
-
-    void bar() const
-    {
-        foo();
-    }
-
-    static int ival;
-
-
-    
-
-private:
-    int mx;
+    static MyClass createObject();
+   
     
 };
 
-int foo()
+class Complex
 {
-    return 2;
-}
+public:
+    //Complex(double r, double i);//overloading deðil
+   // Complex(double a, double d);//overloading deðil so we need a diff kind of ctor to get an object creation so one reason factory function exists for this like beloq
+    static Complex create_polar(double a, double d)
+    {
+        return Complex(a, d, 0);
+    }
+    static Complex create_cartesian(double r, double i)
+    {
+        return Complex(r, i, 0);
+    }
 
-int Nec::ival = ::foo();//ý am telling here to look foo in the global namespace
-// bu variable is static so foo is first lookied in the class scope but if it were a global one we will not do name lookup in class scope
+private:
+  
+    Complex(double , double ); //cartesian
+    Complex(double , double, int ); //polar
+
+};
 
 
 
 
 int main()
 {
-    std::cout << "Nec::ival " << Nec::ival << '\n';
+    auto c1 = Complex::create_cartesian(3.5, 1.2);
+    auto c2 = Complex::create_polar(.2352, 4.5767);
+    
 
 }
