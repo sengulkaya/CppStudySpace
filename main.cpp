2ad28b5bfda3355aa9395fa5534a7a2c84a7eb6b
sengulkaya <sengulkaya15@yahoo.com>
2024-08-02 00:22:33 +0300

    sadece factory function object create edebilsin

--- main.cpp
+++ main.cpp
@@ -8,24 +8,22 @@ public:
     
 };
 
-class Complex
+class DynamicOnly
 {
 public:
-    //Complex(double r, double i);//overloading deðil
-   // Complex(double a, double d);//overloading deðil so we need a diff kind of ctor to get an object creation so one reason factory function exists for this like beloq
-    static Complex create_polar(double a, double d)
-    {
-        return Complex(a, d, 0);
-    }
-    static Complex create_cartesian(double r, double i)
-    {
-        return Complex(r, i, 0);
-    }
+    DynamicOnly(const DynamicOnly&) = delete;
+    DynamicOnly& operator=(const DynamicOnly&) = delete;
 
+    DynamicOnly * create_object()
+    {
+        return new DynamicOnly();
+    }
+   
 private:
-  
-    Complex(double , double ); //cartesian
-    Complex(double , double, int ); //polar
+    DynamicOnly()
+    {
+
+    }
 
 };
 
