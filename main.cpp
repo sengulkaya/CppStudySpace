ee0e900884e7e0fb1295966d019b2b42b4aeb896
sengulkaya <sengulkaya15@yahoo.com>
2024-08-10 19:00:53 +0300

    delegating ctor

--- main.cpp
+++ main.cpp
@@ -1,82 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstdlib>
 
-
-class Fighter
+class MyClass
 {
 public:
-	Fighter()
+	MyClass(int x) : MyClass(x, x, x)
 	{
-		fvec_.push_back(this);
+		
 	}
-	Fighter(std::string name, int age) : name_(std::move(name)), age_(age)
+	MyClass(int x, int y) : MyClass(x, x, 0)
 	{
-		fvec_.push_back(this);
+		
 	}
-	//call by value (string)
-
-	~Fighter()
+	MyClass(int x, int y, int z) : mx(x), my(y), mz(z)
 	{
-		//containerda adresini bul
-		//std::erase(fvec_, this);  C++23
-		if (auto iter = std::find(fvec_.begin(), fvec_.end(), this); iter != fvec_.end()) //linear search
-		{
-			fvec_.erase(iter);
-		}
-		else
-		{
-			std::cerr << "This pointer can not be foun in the container\n";
-		}
+		
 	}
-	Fighter(const Fighter& ref) = delete;
-	Fighter& operator=(const Fighter&) = delete;
-	void ask_for_help()
+	MyClass(const char* p) : MyClass(std::atoi(p))
 	{
-		std::cout << "Ben yýlmaz savaþçý " << name_ <<"yetiþin kardeþlerim, baþým belada...\n";
-		for (auto p : fvec_) //range base for loop
-		{
-			if (p != this && p->get_age() > 15)
-			{
-				std::cout << p->get_name() << ' ';
-			}
-		}
-
-		std::cout << "yardýma koþun hemen\n";
-	}
-	std::string get_name() const
-	{
-		return name_;
-	}
-	int get_age() const
-	{
-		return age_;
+		
 	}
 
 private:
-	std::string name_;
-	inline static std::vector<Fighter*> fvec_; // C++17 default initiliaze edilecek, default ctor will be called
-	int age_{0};//default member initializer
+	void common_code_init()
+	{
+
+	}
+
+	int mx{}, my{}, mz{};
+
 };
-
-
-
-
 
 int main()
 {
-	Fighter f1{"Selami", 50};
-	Fighter f2{ "Nuriye", 25 };
-	Fighter f3{ "Hakan", 22 };
+	//delegating ctor (modern c++)
 
-	
-	Fighter* p1{ new Fighter{"Muzaffer", 30} };
-	Fighter* p2{ new Fighter{"Sabriye", 10} };
-
-	f3.ask_for_help();
-
-	delete p1;
-	delete p2;
-
-	f1.ask_for_help();
 }
