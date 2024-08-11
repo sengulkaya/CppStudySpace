691923aafc622f0a1cb1a818099db09582b3ff82
sengulkaya <sengulkaya15@yahoo.com>
2024-08-11 13:35:39 +0300

    operator overloading intuitive olmalı

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
-#include <cstdlib>
-
-class MyClass
-{
-	//Mesela bu sýnýf sadece bazý private memberlara friendlik vermek istiyorsa?
-	//Attorney - client idiom
-private:
-	int mx, my, mz;
-	void foo();
-	void bar();
-	void baz();
-};
 
 int main()
 {
-	
-	//A B'yi friend declare ederse ve B de C'yi friend declare ederse bu demek deðildir ki dolaylý olarak A C'yi friend declare etmiþ olsun!
-  //
+	//operator overloading intuitive olmalý, client kod için anlaþýlýr olmalý
+	//runtime cost yok bcs overloading kýsmý compiler timeda oluyor
 }
+	
