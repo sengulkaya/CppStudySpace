6dc6b7c0a9a3e6fdd27d680aabcbc9cdf0d0cacb
sengulkaya <sengulkaya15@yahoo.com>
2024-08-11 15:29:30 +0300

    Some operators functions can only be member functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
+//Some operators functions can only be member functions
+// 1- function call operator
+//2- subscriptor operator []
+//3 - assignment operators
+//4 - tür dönüþtürme operators
+//5- (->) arrow operators
+// they dont take default arguments except one
+
 int main()
 {
-	//operator overloading intuitive olmalý, client kod için anlaþýlýr olmalý
-	//runtime cost yok bcs overloading kýsmý compiler timeda oluyor
+	std::string name{ "Mehmet" };
+	name += "Can";
+
+	for (size_t i{}; i < name.size(); ++i)
+	{
+		std::cout << name[i] << ' ';
+	}
 }
 	
