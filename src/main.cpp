cc889851cb819344a2bbd0118e4ec88cac3bcf46
Sengul Kaya <sengulgulsenkaya@gmail.com>
2024-09-24 22:01:08 +0300

    shows initializer_list param ctor chosen over int one when {} used

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
 #include <initializer_list>
+#include <iostream>
+#include <ostream>
 #include <string>
 #include <vector>
 
+class MyClass {
+public:
+   MyClass()
+   {
+      std::cout << "MyClass::MyClass()" << std::endl;
+
+   }
+
+   MyClass(int)
+   {
+      std::cout << "MyClass::MyClass(int)" << std::endl;
+
+   }
+
+   MyClass(int, int)
+   {
+      std::cout << "MyClass::MyClass(int, int)" << std::endl;
+   }
+
+   MyClass(std::initializer_list<int> il)
+   {
+      std::cout << "MyClass::MyClass(std::initializer_list<int>)" << std::endl;
+   }
+
+};
+
 int main()
 {
-    auto x = {1}; //deduced type std::initializer_list<int>
-    auto y{1}; //deduced type int
+   //  {//here no narrowing conversion normally}
+
+   MyClass m1(1);
+
+   MyClass m2{2};
+
+   /*
+   MyClass::MyClass(int)
+   MyClass::MyClass(std::initializer_list<int>)
+   */
+
+   //MyClass m3(3, 4);
+
+   //MyClass m4(5, 6);
+
+
 }
