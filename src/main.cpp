486202b7a1884cc93bfa1099562e4602f3d886be
Sengul Kaya <sengulgulsenkaya@gmail.com>
2024-09-24 21:38:07 +0300

    show two diff type deductions

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,6 @@
 
 int main()
 {
-   const int arr[] = {1, 4, 6, 7, 9};
-   using namespace std;
-   initializer_list<int> ilist{3, 6, 7, 9, 2}; //you can not mutate it is read only
-    //initializer_list<int>::iterator const_iterator = ilist.begin(); //const int *
-   ilist.begin();
-   ilist.size();
-    ilist.end();
-  // const int * iterator = ilist.end();
+    auto x = {1}; //deduced type std::initializer_list<int>
+    auto y{1}; //deduced type int
 }
