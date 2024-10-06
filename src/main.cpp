cbad95bcef0c10ba14039b961c8fd3f24075a0e5
Sengul Kaya <sengulgulsenkaya@gmail.com>
2024-10-06 23:35:55 +0300

    13. Roman to Integer

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
+#include <ostream>
+#include <string>
 
-int a = 1;
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        int sum = 0;
+        char previous;
+        char current;
+        int index = s.length() - 1;
+        while (index >= 0) {
+            char current = s[index--];
+            if (current == 'I') {
+                sum += (previous && (previous == 'V' || previous == 'X')) ? -1 : 1;
+            } else if (current == 'V') {
+                sum += 5;
+            } else if (current == 'X') {
+                sum +=  (previous && (previous == 'L' || previous == 'C')) ? -10 : 10;
+            } else if (current == 'L') {
+                sum += 50;
+            } else if (current == 'C') {
+                sum +=  (previous && (previous == 'D' || previous == 'M')) ? -100 : 100;
+            } else if (current == 'D') {
+                sum += 500;
+            } else if (current == 'M') {
+                sum += 1000;
+            }
+            previous = current;
+        }
+
+        return sum;
+    }
+};
+
 
 int main() {
-    auto f = [](int b) { return a + b; };
-
-    std::cout << f(4);
+    std::string s = "MCDLXXVI";
+    Solution solution;
+    std::cout << solution.romanToInt(s) << std::endl;
 }
-
-/*Only local variables may be captured by a lambda. a is a global variable with static storage duration and may not be captured.
-
-The body of a lambda-expression may refer to local entities of enclosing block scopes by capturing those entities .
-
-Since a is not captured, a in the lambda expression simply refers to the global variable a.
-
-
- */
