75b21c18f8d4f2200fed916441fbe035a94a7181
Sengul Kaya <sengulgulsenkaya@gmail.com>
2024-11-04 21:14:06 +0300

    function overloading

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,23 @@
+
+
 #include <iostream>
-#include <ostream>
-#include <string>
 
-class Solution {
-public:
-    int romanToInt(std::string s) {
-        int sum = 0;
-        char previous;
-        char current;
-        int index = s.length() - 1;
-        while (index >= 0) {
-            char current = s[index--];
-            if (current == 'I') {
-                sum += (previous && (previous == 'V' || previous == 'X')) ? -1 : 1;
-            } else if (current == 'V') {
-                sum += 5;
-            } else if (current == 'X') {
-                sum +=  (previous && (previous == 'L' || previous == 'C')) ? -10 : 10;
-            } else if (current == 'L') {
-                sum += 50;
-            } else if (current == 'C') {
-                sum +=  (previous && (previous == 'D' || previous == 'M')) ? -100 : 100;
-            } else if (current == 'D') {
-                sum += 500;
-            } else if (current == 'M') {
-                sum += 1000;
-            }
-            previous = current;
-        }
+int add(int x, int y)
+{
+   return x + y;
+}
 
-        return sum;
-    }
-};
+
+double add(double x, double y)
+{
+   return x + y;
+}
 
 
 int main() {
-    std::string s = "MCDLXXVI";
-    Solution solution;
-    std::cout << solution.romanToInt(s) << std::endl;
+   std::cout << add(1, 2); // calls add(int, int)
+   std::cout << '\n';
+   std::cout << add(1.2, 3.4); // calls add(double, double)
+
+   return 0;
 }
