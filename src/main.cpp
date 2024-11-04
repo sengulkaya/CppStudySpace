ef6db20995bb2a9b441c7431fcd2827deebeccf4
Sengul Kaya <sengulgulsenkaya@gmail.com>
2024-11-04 22:11:49 +0300

    auto return vs trailing retun type

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 
 template<typename T>
-auto pri1(const T &x) -> decltype(std::cout << x) {
+auto pri1(const T &x) { //-> decltype(std::cout << x) {
     return std::cout << x;
 }
 
 
 int main() {
-    pri1(10); // return val is ostream &
+    pri1(10); // return val is ostream
 }
 
