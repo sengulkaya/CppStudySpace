887ce01fa3de143c1ff652b71055e59436aecf81
Sengul Kaya <sengulgulsenkaya@gmail.com>
2024-11-10 15:51:31 +0300

    Dizgelerin Argüman Olarak Kullanılması

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 
 #include <iostream>
 
-template <class T>
-void print(T a)
+template<class T>
+void display(T &a)
 {
-    std::cout << a << std::endl;
+    for (int k = 0; k < sizeof(a) / sizeof(a[0]); ++k)
+        std::cout << a[k] << " ";
+    std::cout << std::endl;
 }
-
+//Hepsi int türden dizi olmasına rağmen T türü int[3], int[5], int[10] olduğu için
+//compiler 3 ayrı işlev yazmıştır.
 int main()
 {
-    print(3.5);
-    print(30L);
-    print('a');
-    print("Ahmet");
+    int a1[5] = {1, 2, 3, 4, 5};
+    display(a1);
+
+    int a2[3] = {10, 20, 30};
+    display(a2);
+
+    int a3[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    display(a3);
 
     return 0;
 }
