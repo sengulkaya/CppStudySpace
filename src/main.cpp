7110290bcdb8dd412d77ad33ebb17ad1ad6ec028
Sengul Kaya <sengulgulsenkaya@gmail.com>
2024-11-10 21:16:02 +0300

    mechanical string substitutions with almost  no semantic testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
-template<typename T>
-void func(T p)
-{
-    p.foo();
-}
+#include <stdio.h>
+#define SquareOf(x) x*x //macro-call leads to simple string replacements prior to actual compilation
 
-//Bazı derleyiciler işlev şablonuna ilişkin bir çağrı
-//ile karşılaşmadıkça neredeyse hiç bir sözdizim
-//geçerlilik kontrolü yapmaz.
-int main()
-{
-    func(10);	//Geçersiz
+//#define name(dummy1[,dummy2][,...]) tokenstring
 
-    return 0;
+//a macro is a scripted directive for the compiler (rather than a scripted piece of run-time code
+//and therefore it gets handled and done with at compilation time rather than at run time.
+
+#define SquareOf(x) x*x
+int main() {
+    int xin=3;
+    printf("\nxin=%i",xin);
+    printf("\nSquareOf(xin)=%i",SquareOf(xin));
+    printf("\nSquareOf(xin+4)=%i",SquareOf(xin+4));
+    printf("\nSquareOf(xin+xin)=%i",SquareOf(xin+xin));
+
+    /*
+    xin=3
+    SquareOf(xin)=9
+    SquareOf(xin+4)=19
+    SquareOf(xin+xin)=15
+
+    */
 }
